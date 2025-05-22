ad7cebee8a9054366c0b3e88d6570916d6a39048
Canary-Prism <canaryprsn@gmail.com>
2025-05-22 23:49:39 +0800

    uhhhh idk

--- src/main/java/canaryprism/presence/apple/music/TrackContainer.java
+++ src/main/java/canaryprism/presence/apple/music/TrackContainer.java
@@ -4,7 +4,7 @@ public sealed interface TrackContainer permits RealTrack, StoredTrack {
     
     default String getTrackId() {
         return switch (this) {
-            case RealTrack(var track) -> track.getPersistentId();
+            case RealTrack(var track) -> String.valueOf(track.getDatabaseId());
             case StoredTrack(var id) -> id;
         };
     }
